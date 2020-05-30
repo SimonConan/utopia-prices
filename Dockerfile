5746e4d1d875f5a24c72869c607f73cd4a03af09
SimonConan <simonconan@gmail.com>
2020-05-30 18:07:11 +0200

    Structure and add scss

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,10 @@
 FROM wordpress
+
+WORKDIR /var/www/html/wp-content/plugins/utopia-prices
+
+RUN apt-get update
+RUN apt-get -y install curl gnupg
+RUN curl -sL https://deb.nodesource.com/setup_12.x  | bash -
+RUN apt-get -y install nodejs
+RUN npm install
+RUN npm run watch-css
--- utopia-prices/utopia/utopia-prices-table.php
+++ utopia-prices/utopia/utopia-prices-table.php
@@ -1,3 +1,21 @@
 <?php
 
-    echo 'test';
+    add_shortcode('UTOPIA_Prices', 'utopia_prices');
+
+    function utopia_prices() {
+
+        // Register style sheet.
+        add_action( 'wp_enqueue_scripts', 'register_utopia_prices_styles' );
+        do_action( 'wp_enqueue_scripts');
+
+        ob_start();
+        include UTOPIA_PLUGIN_PATH . "utopia/assets/template.html";
+        return ob_get_clean();
+
+    }
+
+    function register_utopia_prices_styles() {
+        wp_register_style( 'utopia-prices', UTOPIA_PLUGIN_URL . '/utopia/assets/css/main.css' );
+        wp_enqueue_style( 'utopia-prices' );
+    }
+  
